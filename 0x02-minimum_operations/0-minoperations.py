e1e25b85e4ac3cabaf485e1878b24fa4aadf32f0
Jamezfn <jamesmakachia19@gmail.com>
2025-08-16 16:46:31 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,36 +1,16 @@
-#!/usr/bin/python3
-"""
-Minimum Operations
-"""
+#!/usr/bin/env python
+"""Minimum Operations"""
+
 
 def minOperations(n):
-    """
-    Computes the fewest number of operations needed to result
-    in exactly n H characters.
-    """
-    if not isinstance(n, int):
+    """Number of operations"""
+    if n <= 1:
         return 0
-
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    # print('H', end='')
-    while done  < n:
-        if clipboard == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            ops_count + 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
-
-    return ops_count
+    operations = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
